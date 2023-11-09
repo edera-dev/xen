c58ecc9b8d1d5daf525ea8528e0f0f9eb00ce13b
Stewart Hildebrand <stewart.hildebrand@amd.com>
2023-11-09 14:51:33 -0500

    [WIP] xen/arm: map ITS doorbell registers for domUs
    Map a guest ITS doorbell for domUs at GUEST_GICV3_ITS_BASE +
    ITS_DOORBELL_OFFSET. Map it to first real ITS doorbell found in our list
    of hardware ITSes, so at the moment only hardware with a single ITS is
    supported.
    
    Remove BUG_ON() from arm_iommu_map_page() to allow translated iommu
    mappings.
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    * use translated mapping for ITS doorbell register
    * update commit description
    * break out of hw_its loop after first iteration

--- xen/drivers/passthrough/arm/iommu_helpers.c
+++ xen/drivers/passthrough/arm/iommu_helpers.c
@@ -36,6 +36,14 @@ int __must_check arm_iommu_map_page(struct domain *d, dfn_t dfn, mfn_t mfn,
 {
     p2m_type_t t;
 
+    /*
+     * Grant mappings can be used for DMA requests. The dev_bus_addr
+     * returned by the hypercall is the MFN (not the IPA). For device
+     * protected by an IOMMU, Xen needs to add a 1:1 mapping in the domain
+     * p2m to allow DMA request to work.
+     * For ITS doorbell mappings, translation (dfn != mfn) is allowed.
+     */
+
     /* We only support readable and writable flags */
     if ( !(flags & (IOMMUF_readable | IOMMUF_writable)) )
         return -EINVAL;
