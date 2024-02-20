31d3ae8738ac6f5c31997085bbf60884b3bdd785
Stefano Stabellini <stefano.stabellini@amd.com>
2024-02-20 15:36:19 -0500

    xen/pci: use has_vpci_bridge in vpci and pci
    Use has_vpci_bridge instead of is_hardware_domain checks in
    vpci_sbdf_from_gpa, domain_vpci_init, domain_vpci_get_num_mmio_handlers,
    pci_get_pdev.
    
    The presence or absence of a bridge no longer corresponds to whether the
    domain is_hardware_domain, so remove the now invalid ASSERTs.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    This was probably true at some point in the past, in an old xlnx_rebase_4.* branch:
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/drivers/vpci/vpci.c
+++ xen/drivers/vpci/vpci.c
@@ -732,7 +732,7 @@ uint32_t vpci_read(pci_sbdf_t sbdf, unsigned int reg, unsigned int size)
      * pci_lock is sufficient.
      */
     read_lock(&d->pci_lock);
-    if ( is_hardware_domain(d) )
+    if ( !has_vpci_bridge(d) )
     {
         pdev = pci_get_pdev(d, sbdf);
         if ( !pdev )
@@ -859,7 +859,7 @@ void vpci_write(pci_sbdf_t sbdf, unsigned int reg, unsigned int size,
      * are modifying BARs, so there is a room for improvement.
      */
     write_lock(&d->pci_lock);
-    if ( is_hardware_domain(d) )
+    if ( !has_vpci_bridge(d) )
     {
         pdev = pci_get_pdev(d, sbdf);
         if ( !pdev )
