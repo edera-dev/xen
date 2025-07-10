d22ddef8faced26bdbbddd5321c79e6d444c6e56
Stewart Hildebrand <stewart.hildebrand@amd.com>
2025-07-10 15:04:12 -0400

    [WIP] sr-iov domU

--- xen/drivers/vpci/sriov.c
+++ xen/drivers/vpci/sriov.c
@@ -51,7 +51,8 @@ static int vf_init_bars(const struct pci_dev *vf_pdev)
     for ( i = 0; i < PCI_SRIOV_NUM_BARS; i++)
     {
         bars[i].addr = physfn_vf_bars[i].addr + vf_idx * physfn_vf_bars[i].size;
-        bars[i].guest_addr = bars[i].addr;
+        if ( pf_pdev->domain == vf_pdev->domain || bars[i].guest_addr == 0 )
+            bars[i].guest_addr = bars[i].addr;
         bars[i].size = physfn_vf_bars[i].size;
         bars[i].type = physfn_vf_bars[i].type;
         bars[i].prefetchable = physfn_vf_bars[i].prefetchable;
@@ -167,10 +168,49 @@ static void cf_check control_write(const struct pci_dev *pdev, unsigned int reg,
             list_for_each_entry(vf_pdev, &pdev->vf_list, vf_list)
                 map_vf(vf_pdev, PCI_COMMAND_MEMORY, VPCI_MAP);
         }
+        /* TODO: unmap vf */
     }
 
     pci_conf_write16(pdev->sbdf, reg, val);
 }
+
+#ifdef CONFIG_HAS_VPCI_GUEST_SUPPORT
+static void cf_check vf_cmd_write(
+    const struct pci_dev *pdev, unsigned int reg, uint32_t cmd, void *data)
+{
+    struct vpci_header *header = data;
+
+    cmd |= PCI_COMMAND_INTX_DISABLE;
+
+    header->guest_cmd = cmd;
+
+    /*
+     * Let Dom0 play with all the bits directly except for the memory
+     * decoding one. Bits that are not allowed for DomU are already
+     * handled above and by the rsvdp_mask.
+     */
+    if ( header->bars_mapped != !!(cmd & PCI_COMMAND_MEMORY) )
+    {
+        /*
+         * Ignore the error. No memory has been added or removed from the p2m
+         * (because the actual p2m changes are deferred in defer_map) and the
+         * memory decoding bit has not been changed, so leave everything as-is,
+         * hoping the guest will realize and try again.
+         */
+        map_vf(pdev, cmd, cmd & PCI_COMMAND_MEMORY ? VPCI_MAP : VPCI_UNMAP);
+    }
+    else
+        pci_conf_write16(pdev->sbdf, reg, cmd);
+}
+
+static uint32_t cf_check vf_cmd_read(
+    const struct pci_dev *pdev, unsigned int reg, void *data)
+{
+    const struct vpci_header *header = data;
+
+    return header->guest_cmd;
+}
+#endif /* CONFIG_HAS_VPCI_GUEST_SUPPORT */
 
 static int vf_init_header(struct pci_dev *vf_pdev)
 {
@@ -189,6 +229,82 @@ static int vf_init_header(struct pci_dev *vf_pdev)
 
     sriov_pos = pci_find_ext_capability(pf_pdev->sbdf, PCI_EXT_CAP_ID_SRIOV);
     ctrl = pci_conf_read16(pf_pdev->sbdf, sriov_pos + PCI_SRIOV_CTRL);
+
+#ifdef CONFIG_HAS_VPCI_GUEST_SUPPORT
+    if ( pf_pdev->domain != vf_pdev->domain /* TODO: !hw_dom? */)
+    {
+        uint16_t vid = pci_conf_read16(pf_pdev->sbdf, PCI_VENDOR_ID);
+        uint16_t did = pci_conf_read16(pf_pdev->sbdf, sriov_pos + PCI_SRIOV_VF_DID);
+        struct vpci_bar *bars = vf_pdev->vpci->header.bars;
+        unsigned int i;
+
+        rc = vpci_add_register(vf_pdev->vpci, vpci_read_val, NULL,
+                               PCI_VENDOR_ID, 2,
+                               (void *)(uintptr_t)vid);
+        if ( rc )
+            return rc;
+
+        rc = vpci_add_register(vf_pdev->vpci, vpci_read_val, NULL,
+                               PCI_DEVICE_ID, 2,
+                               (void *)(uintptr_t)did);
+        if ( rc )
+            return rc;
+
+        /* Hardcode multi-function device bit to 0 */
+        rc = vpci_add_register(vf_pdev->vpci, vpci_read_val, NULL,
+                               PCI_HEADER_TYPE, 1,
+                               (void *)PCI_HEADER_TYPE_NORMAL);
+        if ( rc )
+            return rc;
+
+        /* TODO: look at the VF ROZ bits in the pci spec */
+        rc = vpci_add_register_mask(vf_pdev->vpci,
+                                    vf_cmd_read,
+                                    vf_cmd_write, PCI_COMMAND, 2,
+                                    &vf_pdev->vpci->header, 0, 0,
+                                               PCI_COMMAND_RSVDP_MASK |
+                                               PCI_COMMAND_IO |
+                                               PCI_COMMAND_PARITY |
+                                               PCI_COMMAND_WAIT |
+                                               PCI_COMMAND_SERR |
+                                               PCI_COMMAND_FAST_BACK,
+                                    0);
+        if ( rc )
+            return rc;
+
+        rc = vpci_init_capability_list(vf_pdev);
+        if ( rc )
+            return rc;
+
+        for ( i = 0; i < PCI_SRIOV_NUM_BARS; i++ )
+        {
+            switch ( pf_pdev->vpci->sriov->vf_bars[i].type )
+            {
+            case VPCI_BAR_MEM32:
+            case VPCI_BAR_MEM64_LO:
+            case VPCI_BAR_MEM64_HI:
+                rc = vpci_add_register(vf_pdev->vpci,
+                                       vpci_guest_mem_bar_read,
+                                       vpci_guest_mem_bar_write,
+                                       PCI_BASE_ADDRESS_0 + i * 4, 4, &bars[i]);
+                if ( rc )
+                    return rc;
+                break;
+            default:
+                rc = vpci_add_register(vf_pdev->vpci, vpci_read_val, NULL,
+                                       PCI_BASE_ADDRESS_0 + i * 4, 4,
+                                       (void *)0);
+                if ( rc )
+                    return rc;
+                break;
+            }
+        }
+
+        rc = vf_init_bars(vf_pdev);
+        if ( rc )
+            return rc;
+    }
+#endif /* CONFIG_HAS_VPCI_GUEST_SUPPORT */
 
     if ( (pf_pdev->domain == vf_pdev->domain) && (ctrl & PCI_SRIOV_CTRL_MSE) )
     {
